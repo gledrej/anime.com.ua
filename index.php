f8e01d87a1e300eb9ccc0d1906284eb1ce8070b3
Gledrej <gledrej1988@gmail.com>
2015-12-07 21:33:21 +0200

    style

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+    <script src="/js/script.js" type="text/javascript" charset="utf-8"></script>
+    <link rel="stylesheet" media="all" type="text/css" href="/css/normalize.css">
+    <link rel="stylesheet" media="all" type="text/css" href="/css/style.css">
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="block-1-header-menu" style="background-color: yellow;color: black;">
+        <div class="block-1-header-menu-logo float-left" style="background-color: aqua;color: black;">
+            <div class="block-1-header-menu-logo-img text-center float-left">logo</div>
+            <div class="block-1-header-menu-logo-text text-center float-left">text</div>
+        </div>
+        <div class="block-1-header-menu-list float-right">
+            <div class="block-1-header-menu-list-li text-center float-left">test</div>
+            <div class="block-1-header-menu-list-li text-center float-left">test</div>
+            <div class="block-1-header-menu-list-li text-center float-left">test</div>
+            <div class="block-1-header-menu-list-li text-center float-left">test</div>
+
+        </div>
+        <div></div>
+
+    </div>
+
+</div>
+
+<div class="footer">
+    test
+</div>
+
+</body>
+</html>
