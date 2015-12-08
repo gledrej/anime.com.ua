608e9061f2a70de1bccb8f1e6595bc13851aa397
Gledrej <gledrej1988@gmail.com>
2015-12-08 14:53:14 +0200

    sait

--- index.php
+++ index.php
@@ -1,27 +1,55 @@
 <html>
 <head>
-    <script src="/js/script.js" type="text/javascript" charset="utf-8"></script>
-    <link rel="stylesheet" media="all" type="text/css" href="/css/normalize.css">
+
+    <script type="text/javascript" charset="utf-8" src="/js/script.js"></script>
+    <script type="text/javascript" charset="utf-8" src="/js/owl.carousel.js"></script>
+    <script type="text/javascript" charset="utf-8" src="/js/owl.carousel.min.js"></script>
+
     <link rel="stylesheet" media="all" type="text/css" href="/css/style.css">
+    <link rel="stylesheet" media="all" type="text/css" href="/css/normalize.css">
+    <link rel="stylesheet" media="all" type="text/css" href="/css/owl.carousel.css">
+
 </head>
 <body>
 
 <div class="wrapper">
+
+
     <div class="block-1-header-menu" style="background-color: yellow;color: black;">
-        <div class="block-1-header-menu-logo float-left" style="background-color: aqua;color: black;">
+        <div class="block-1-header-menu-logo" style="background-color: aqua;color: black;">
             <div class="block-1-header-menu-logo-img text-center float-left">logo</div>
             <div class="block-1-header-menu-logo-text text-center float-left">text</div>
         </div>
-        <div class="block-1-header-menu-list float-right">
+        <div class="block-1-header-menu-list">
             <div class="block-1-header-menu-list-li text-center float-left">test</div>
             <div class="block-1-header-menu-list-li text-center float-left">test</div>
             <div class="block-1-header-menu-list-li text-center float-left">test</div>
             <div class="block-1-header-menu-list-li text-center float-left">test</div>
-
         </div>
         <div></div>
+    </div>
+
+
+    <div class="block-2-header-img">
+        <div class="block-2-header-img-login">
+            test
+        </div>
 
     </div>
+
+
+    <div class="block-3">
+        <div class="block-3-banner-1"></div>
+        <div class="block-3-slaider-1">
+
+
+
+        </div>
+
+
+    </div>
+
+
 
 </div>
 
